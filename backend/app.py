5d933d2045e76d1af4aaaaee27d0424bfbe95600
Youssuf Helaly <yhela099@uottawa.ca>
2024-09-30 17:17:28 -0400

    better documentation

--- backend/app.py
+++ backend/app.py
@@ -17,26 +17,30 @@ from auth import auth_routes
 
 app = Flask(__name__, static_folder='build', static_url_path='')
 
-
 # Load configuration from config.py
 app.config.from_object(Config)
 
 # Set SECRET_KEY to a secret key
-# The value is retrieved from the environment variables
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-# Initialize extensions
+# Initialize the SQLAlchemy database instance with the Flask app
 db.init_app(app)
+
+# Initialize the login manager to manage user sessions and authentication
 login_manager.init_app(app)
+
+# Initialize Bcrypt for hashing and verifying user passwords securely
 bcrypt.init_app(app)
+
+# Initialize JWT to handle user authentication and authorization through tokens
 jwt.init_app(app)
+
+# Enable Cross-Origin Resource Sharing (CORS) to allow requests from different origins
 CORS(app)
 
 # Logging configuration
-# Set the logging level to DEBUG
 logging.basicConfig(level=logging.DEBUG)
 
-# Register Blueprints (routes)
 # Register the image_routes blueprint under the '/images' prefix
 app.register_blueprint(image_routes, url_prefix='/images')
 
@@ -44,25 +48,12 @@ app.register_blueprint(image_routes, url_prefix='/images')
 app.register_blueprint(auth_routes, url_prefix='/auth')
 
 # Initialize the database
-# This is done in an application context to ensure
-# that the database is initialized correctly
 with app.app_context():
     db.create_all()
 
-# Static File Serving (React Frontend)
-# This is the main entry point for the React frontend
-# It serves static files from the 'build' folder
 @app.route('/')
-@app.route('/<path:path>')
-def serve_react_app(path=''):
-    """
-    Serves static files from the 'build' folder.
-
-    If the requested path is a file that exists in the 'build' folder,
-    it is served directly. Otherwise, it serves the 'index.html' file.
-    """
-    if path and os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
+def serve():
+    """Serve the React frontend from the static folder."""
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
